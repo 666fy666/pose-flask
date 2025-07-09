588fe23a2f05b4796baf10fb8d66456f860022a3
syy <1327623813@qq.com>
2025-07-09 14:10:23 +0800

    更新readme文档

--- app.py
+++ app.py
@@ -1147,4 +1147,4 @@ def api_delete_patient_report(patient_id, filename):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5051) 
+    app.run(debug=True, host='0.0.0.0', port=5050) 
